2082a3b8c55389494f76106882a5ced5f517dc01
Jimmy Mattsson <jimmy.mattsson@tretton37.com>
2024-09-06 12:31:48 +0200

    Make class static

--- ThePlaylist.Core/Specification/Specifications.cs
+++ ThePlaylist.Core/Specification/Specifications.cs
@@ -1,6 +1,6 @@
 ﻿namespace ThePlaylist.Core.Specification;
 
-public class Specifications
+public static class Specifications
 {
     public static SpecificationBuilder<Entitites.Genre> Genre => new SpecificationBuilder<Entitites.Genre>();
 }
