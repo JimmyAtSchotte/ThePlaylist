5a8b5dc2f2004561e1a25785cb169c9e0c917dbf
Jimmy Mattsson <jimmy.mattsson@tretton37.com>
2024-09-16 10:54:37 +0200

    Better error message

--- ThePlaylist.Specifications/CriteriaSpecification.cs
+++ ThePlaylist.Specifications/CriteriaSpecification.cs
@@ -6,14 +6,13 @@ namespace ThePlaylist.Specifications;
 public class CriteriaSpecification<T> : Specification<T>
 {
     private Action<ICriteria>? _action;
-    public Action<ICriteria> GetCriteria() => _action ?? (_ => {});
+    public Action<ICriteria> GetCriteria() => _action ?? throw new NotSupportedException("The criteria has not been specified. Please use UseCriteria() to define the criteria.");
     protected void UseCriteria(Action<ICriteria> action) => _action = action;
-    
 }
 
 public class CriteriaSpecification<T, TResult> : Specification<T, TResult>
 {
     private Action<ICriteria>? _action;
-    public Action<ICriteria> GetCriteria() => _action ?? (_ => {});
+    public Action<ICriteria> GetCriteria() => _action ?? throw new NotSupportedException("The criteria has not been specified. Please use UseCriteria() to define the criteria.");
     protected void UseCriteria(Action<ICriteria> action) => _action = action;
 }
--- ThePlaylist.Specifications/QueryOverSpecification.cs
+++ ThePlaylist.Specifications/QueryOverSpecification.cs
@@ -6,13 +6,13 @@ namespace ThePlaylist.Specifications;
 public class QueryOverSpecification<T> : Specification<T>
 {
     private Action<IQueryOver<T, T>>? _action;
-    public Action<IQueryOver<T, T>> GetQueryOver() => _action ?? (_ => {});
+    public Action<IQueryOver<T, T>> GetQueryOver() => _action ?? throw new NotSupportedException("The Query over has not been specified. Please use the UseQueryOver() to define the query over.");
     protected void UseQueryOver(Action<IQueryOver<T, T>> action) => _action = action;
 }
 
 public class QueryOverSpecification<T, TResult> : Specification<T, TResult>
 {
     private Func<IQueryOver<T, T>, IQueryOver<T, T>>? _action;
-    public Func<IQueryOver<T, T>, IQueryOver<T, T>> GetQueryOver() => _action ??  throw new NotSupportedException();
+    public Func<IQueryOver<T, T>, IQueryOver<T, T>> GetQueryOver() => _action ??  throw new NotSupportedException("The Query over has not been specified. Please use the UseQueryOver() to define the query over.");
     protected void UseQueryOver(Func<IQueryOver<T, T>, IQueryOver<T, T>> action) => _action = action;
 }
