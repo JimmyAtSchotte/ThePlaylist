d80f3e23c5960612736755b3f0857bc5277d0779
Jimmy Mattsson <jimmy.mattsson@tretton37.com>
2024-09-26 14:16:02 +0200

    Projection as T to a specification

---
+++ ThePlaylist.Infrastructure.Tests/Projections/PlaylistName.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using ThePlaylist.Infrastructure.Tests.__TestCaseSources.RepositorySource;
+using ThePlaylist.Specifications;
+using ThePlaylist.Specifications.Entitites.Track.Query;
+
+namespace ThePlaylist.Infrastructure.Tests.Projections;
+
+[TestFixture]
+public class TrackName
+{
+    [TestCaseSource(typeof(RepositorySources), nameof(RepositorySources.RepositoryProviders))]
+    public void Projection(IRepositorySource repositoryProvider)
+    {
+        var track = new Core.Entitites.Track
+        {
+            Name = Guid.NewGuid().ToString()
+        };
+        
+        using var repository = repositoryProvider.CreateRepository();
+        repository.Add(track);
+
+        var specification = Specs.Track.TrackByName<Core.Projections.TrackName>(track.Name,
+            x => new Core.Projections.TrackName()
+            {
+                Name = x.Name
+            });
+        
+        var list = repository.List(specification);
+        
+        list.Should().Contain(x => x.Name == track.Name);
+    }
+    
+    [TestCaseSource(typeof(RepositorySources), nameof(RepositorySources.RepositoryProviders))]
+    public async Task ProjectionAsync(IRepositorySource repositoryProvider)
+    {
+        var track = new Core.Entitites.Track
+        {
+            Name = Guid.NewGuid().ToString()
+        };
+        
+        await using var repository = repositoryProvider.CreateRepository();
+        await repository.AddAsync(track, CancellationToken.None);
+        
+        
+        var specification = Specs.Track.TrackByName<Core.Projections.TrackName>(track.Name,
+        x => new Core.Projections.TrackName()
+        {
+            Name = x.Name
+        });
+        
+        var list = await repository.ListAsync(specification, CancellationToken.None);
+        
+        list.Should().Contain(x => x.Name == track.Name);
+    }
+    
+    [TestCaseSource(typeof(RepositorySources), nameof(RepositorySources.RepositoryProviders))]
+    public void AnnonumusProjection(IRepositorySource repositoryProvider)
+    {
+        var track = new Core.Entitites.Track
+        {
+            Name = Guid.NewGuid().ToString()
+        };
+        
+        using var repository = repositoryProvider.CreateRepository();
+        repository.Add(track);
+
+        var specification = Specs.Track.TrackByName<Core.Projections.TrackName>(track.Name,
+        x => new ()
+        {
+            Name = x.Name
+        });
+        
+        var list = repository.List(specification);
+        
+        list.Should().Contain(x => x.Name == track.Name);
+    }
+}
--- ThePlaylist.Specifications/Entitites/Track/Query/TrackByName.cs
+++ ThePlaylist.Specifications/Entitites/Track/Query/TrackByName.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using Ardalis.Specification;
 
 namespace ThePlaylist.Specifications.Entitites.Track.Query;
@@ -5,15 +6,33 @@ namespace ThePlaylist.Specifications.Entitites.Track.Query;
 
 public static partial class SpecificationSetExtensions
 {
+    private static Expression<Func<Core.Entitites.Track, bool>> Predicate(string name)
+    {
+        return x => x.Name == name;
+    }
+    
     public static Specification<Core.Entitites.Track> TrackByName(
-        this SpecificationSet<Core.Entitites.Track> set, string name)
-        => new TrackByName(name);
+        this SpecificationSet<Core.Entitites.Track> _, string name)
+        => new TrackByName(Predicate(name));
+    
+    public static Specification<Core.Entitites.Track, TProjection> TrackByName<TProjection>(
+        this SpecificationSet<Core.Entitites.Track> _, string name, Expression<Func<Core.Entitites.Track, TProjection>> selector)
+        => new TrackByName<TProjection>(Predicate(name), selector);
 }
 
 internal sealed class TrackByName : Specification<Core.Entitites.Track>
 {
-    public TrackByName(string trackName)
+    public TrackByName(Expression<Func<Core.Entitites.Track, bool>> predicate)
     {
-        Query.Where(x => x.Name == trackName);
+        Query.Where(predicate);
+    }   
+}
+
+internal sealed class TrackByName<TProjection> : Specification<Core.Entitites.Track, TProjection>
+{
+    public TrackByName(Expression<Func<Core.Entitites.Track, bool>> predicate, Expression<Func<Core.Entitites.Track, TProjection>> selector)
+    {
+        Query.Where(predicate);
+        Query.Select(selector);
     }
 }
