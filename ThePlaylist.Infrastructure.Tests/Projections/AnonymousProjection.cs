1316249384129316b7df954efe2fb16b35f354b0
Jimmy Mattsson <jimmy.mattsson@tretton37.com>
2024-09-26 14:24:59 +0200

    Test anonymous projection

---
+++ ThePlaylist.Infrastructure.Tests/Projections/AnonymousProjection.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using ThePlaylist.Infrastructure.Tests.__TestCaseSources.RepositorySource;
+using ThePlaylist.Specifications;
+using ThePlaylist.Specifications.Entitites.Track.Query;
+
+namespace ThePlaylist.Infrastructure.Tests.Projections;
+
+[TestFixture]
+public class AnonymousProjection
+{
+    [TestCaseSource(typeof(RepositorySources), nameof(RepositorySources.RepositoryProviders))]
+    public void TrackByName(IRepositorySource repositoryProvider)
+    {
+        var track = new Core.Entitites.Track
+        {
+            Name = Guid.NewGuid().ToString()
+        };
+        
+        using var repository = repositoryProvider.CreateRepository();
+        repository.Add(track);
+
+        var specification = Specs.Track.TrackByName(track.Name,
+        x => new 
+        {
+            x.Name
+        });
+        
+        var list = repository.List(specification);
+        
+        list.Should().Contain(x => x.Name == track.Name);
+    }
+}
